cee207ef360f5494329a10f8d43558fe6ca8b14f
ksaga <sagalaptop@outlook.jp>
2025-07-11 18:30:57 +0900

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "monaco-code-editor",
+  "version": "0.1.0",
+  "private": true,
+  "dependencies": {
+    "lucide-react": "^0.263.1",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-scripts": "5.0.1"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "CI=false react-scripts build"
+  },
+  "devDependencies": {
+    "tailwindcss": "^3.3.6"
+  }
+}
